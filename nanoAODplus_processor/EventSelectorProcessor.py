6690f68e858ad6c7ff1a739420bca9bcfc06ab72
Kevin Mota Amarilo <kevin.amarilo@cern.ch>
2020-11-12 23:21:08 +0100

    change EventSelector for Dimu collection

--- nanoAODplus_processor/EventSelectorProcessor.py
+++ nanoAODplus_processor/EventSelectorProcessor.py
@@ -1,6 +1,6 @@
 from coffea.analysis_objects import JaggedCandidateArray
 import coffea.processor as processor
-from awkward import JaggedArray
+from awkward import JaggedArray, Table
 import numpy as np
 from coffea.util import save, load
 import random
@@ -20,9 +20,33 @@ class EventSelectorProcessor(processor.ProcessorABC):
     
    def process(self, df):
       output = self.accumulator.identity()
-      
-      # Muon candidates
+
       output['dataset'] = df['dataset']
+
+      # Dimu candidates
+      if df['nDimu'].size != 0:
+         Dimu = JaggedCandidateArray.candidatesfromcounts(
+               df['nDimu'],
+               pt=df['Dimu_pt'],
+               eta=df['Dimu_eta'],
+               phi=df['Dimu_phi'],
+               mass=df['Dimu_mass'],
+               charge=df['Dimu_charge'],
+               t1Idx=df['Dimut1_muIdx'],
+               t2Idx=df['Dimut2_muIdx'],
+               )
+      else:
+         Dimu = JaggedCandidateArray.candidatesfromcounts(
+               np.array([]),
+               pt=np.array([]),
+               eta=np.array([]),
+               phi=np.array([]),
+               mass=np.array([]),
+               charge=np.array([]),
+               )
+
+
+      # Muon candidates
       if df['nMuon'].size != 0:
          Muon = JaggedCandidateArray.candidatesfromcounts(
                df['nMuon'],
@@ -33,11 +57,6 @@ class EventSelectorProcessor(processor.ProcessorABC):
                charge=df['Muon_charge'],
                isGlobal=df['Muon_isGlobal'],
                softId=df['Muon_softId'],
-               vtxIdx=df['Muon_vtxIdx'],
-               pfRelIso04_all=df['Muon_pfRelIso04_all'],
-               x=df['Muon_x'],
-               y=df['Muon_y'],
-               z=df['Muon_z'],
                )
       else:  
          Muon = JaggedCandidateArray.candidatesfromcounts(
@@ -49,14 +68,9 @@ class EventSelectorProcessor(processor.ProcessorABC):
                charge=np.array([]),
                isGlobal=np.array([]),
                softId=np.array([]),
-               vtxIdx=np.array([]),
-               pfRelIso04_all=np.array([]),
-               x=np.array([]),
-               y=np.array([]),
-               z=np.array([]),
                )        
       
-      # Dzero candidates
+      # D0 candidates
       if df['nD0'].size != 0:
          D0 = JaggedCandidateArray.candidatesfromcounts(
                df['nD0'],
@@ -82,6 +96,7 @@ class EventSelectorProcessor(processor.ProcessorABC):
                z=np.array([]),
                )
 
+      # Dstar candidates
       if df['nDstar'].size != 0:
          Dstar = JaggedCandidateArray.candidatesfromcounts(
                df['nDstar'],
@@ -108,84 +123,109 @@ class EventSelectorProcessor(processor.ProcessorABC):
                )
 
       output['cutflow']['all events']  += Muon.size
-      output['cutflow']['all muons']   += Muon.counts.sum()
+      output['cutflow']['all Dimu']    += Dimu.counts.sum()
       output['cutflow']['all D0']      += D0.counts.sum()
       output['cutflow']['all Dstar']   += Dstar.counts.sum()
+
+      # Dimu cuts: charge = 0...
+      Dimu = Dimu[Dimu.charge == 0]
+      output['cutflow']['Dimu 0 charge'] += Dimu.counts.sum()
+
+      mass_cut = (Dimu.mass > 8.5) & (Dimu.mass < 11.5)
+      Dimu = Dimu[mass_cut]
+      output['cutflow']['Upsilon mass'] += Dimu.counts.sum()
+
+      ndimu_cut = Dimu.counts > 0
+      Dimu = Dimu[ndimu_cut]
+      Muon = Muon[ndimu_cut]
+      D0 = D0[ndimu_cut]
+      Dstar = Dstar[ndimu_cut]
+
+      ############### Get the Muons from Dimu, for cuts in their params
+
+      # For Muon 1:
+      mu1Idx = (Dimu.t1Idx + Muon.starts).content
+      mu2Idx = (Dimu.t2Idx + Muon.starts).content
+      Muon1 = JaggedCandidateArray.fromoffsets(Dimu.offsets, Muon.content[mu1Idx])
+      Muon2 = JaggedCandidateArray.fromoffsets(Dimu.offsets, Muon.content[mu2Idx])
+
+      # SoftId and Global Muon cuts
+      soft_id1 = (Muon1.softId > 0)
+      Dimu = Dimu[soft_id1]
+      Muon1 = Muon1[soft_id1]
+      Muon2 = Muon2[soft_id1]
+      output['cutflow']['Dimu muon1 softId'] += Dimu.counts.sum()
+
+      soft_id2 = (Muon2.softId > 0)
+      Dimu = Dimu[soft_id2]
+      Muon1 = Muon1[soft_id2]
+      Muon2 = Muon2[soft_id2]
+      output['cutflow']['Dimu muon2 softId'] += Dimu.counts.sum()
+
+      global_muon1 = (Muon1.isGlobal > 0)
+      Dimu = Dimu[global_muon1]
+      Muon1 = Muon1[global_muon1]
+      Muon2 = Muon2[global_muon1]
+      output['cutflow']['Dimu muon1 global'] += Dimu.counts.sum()
+
+      global_muon2 = (Muon2.isGlobal > 0)
+      Dimu = Dimu[global_muon2]
+      Muon1 = Muon1[global_muon2]
+      Muon2 = Muon2[global_muon2]
+      output['cutflow']['Dimu muon2 global'] += Dimu.counts.sum()
+
+      # pt and eta cuts
+      pt_cut1 = (Muon1.pt > 3)
+      Dimu = Dimu[pt_cut1]
+      Muon1 = Muon1[pt_cut1]
+      Muon2 = Muon2[pt_cut1]
+      output['cutflow']['Muon1 pt cut'] += Dimu.counts.sum()
+
+      pt_cut2 = (Muon2.pt > 3)
+      Dimu = Dimu[pt_cut2]
+      Muon1 = Muon1[pt_cut2]
+      Muon2 = Muon2[pt_cut2]
+      output['cutflow']['Muon2 pt cut'] += Dimu.counts.sum()
+
+      eta_cut1 = (np.absolute(Muon1.eta) <= 2.4)
+      Dimu = Dimu[eta_cut1]
+      Muon1 = Muon1[eta_cut1]
+      Muon2 = Muon2[eta_cut1]
+      output['cutflow']['Muon1 eta cut'] += Dimu.counts.sum()
+
+      eta_cut2 = (np.absolute(Muon2.eta) <= 2.4)
+      Dimu = Dimu[eta_cut2]
+      Muon1 = Muon1[eta_cut2]
+      Muon2 = Muon2[eta_cut2]
+      output['cutflow']['Muon2 eta cut'] += Dimu.counts.sum()
+
+      # Cuts into events with at least 1 Dimu
+      ndimu_cut = Dimu.counts > 0
+      Dimu = Dimu[ndimu_cut]
+      Muon1 = Muon1[ndimu_cut]
+      Muon2 = Muon2[ndimu_cut]
+      D0 = D0[ndimu_cut]
+      Dstar = Dstar[ndimu_cut]
+
+      output['cutflow']['Dimu final']    += Dimu.counts.sum()
+      output['cutflow']['D0 final']      += D0.counts.sum()
+      output['cutflow']['Dstar final']   += Dstar.counts.sum()
       
-      # global and soft muon
-      soft_id = (Muon.softId > 0)
-      Muon = Muon[soft_id]
-      output['cutflow']['soft muon'] += soft_id.sum().sum()
+      ############### Leading and Trailing muon separation
+      leading_mu = (Muon1.pt.content > Muon2.pt.content)
+      Muon_lead = JaggedCandidateArray.candidatesfromoffsets(Dimu.offsets, 
+                                                       pt=np.where(leading_mu, Muon1.pt.content, Muon2.pt.content),
+                                                       eta=np.where(leading_mu, Muon1.eta.content, Muon2.eta.content),
+                                                       phi=np.where(leading_mu, Muon1.phi.content, Muon2.phi.content),
+                                                       mass=np.where(leading_mu, Muon1.mass.content, Muon2.mass.content),)
 
-      global_muon = (Muon.isGlobal > 0)
-      Muon = Muon[global_muon]
-      output['cutflow']['global muon'] += global_muon.sum().sum()
+      Muon_trail = JaggedCandidateArray.candidatesfromoffsets(Dimu.offsets, 
+                                                       pt=np.where(~leading_mu, Muon1.pt.content, Muon2.pt.content),
+                                                       eta=np.where(~leading_mu, Muon1.eta.content, Muon2.eta.content),
+                                                       phi=np.where(~leading_mu, Muon1.phi.content, Muon2.phi.content),
+                                                       mass=np.where(~leading_mu, Muon1.mass.content, Muon2.mass.content),)
 
-      #pt and eta cuts
-      pt_cut = (Muon.pt > 3)
-      Muon = Muon[pt_cut]
-      output['cutflow']['pt cut'] += Muon.counts.sum()
-
-      eta_cut = (np.absolute(Muon.eta) <= 2.4)
-      Muon = Muon[eta_cut]
-      output['cutflow']['eta cut'] += Muon.counts.sum()
-
-      #isolated muon
-      iso_muon = (Muon.pfRelIso04_all < 0.4)
-      Muon = Muon[iso_muon]
-      output['cutflow']['iso muon'] += Muon.counts.sum()
-
-      #valid vtx
-      valid_vtx = (Muon.vtxIdx != -1)
-      Muon = Muon[valid_vtx]
-      output['cutflow']['valid vtx'] += Muon.counts.sum()
-
-      #dimuon
-      twomuons = (Muon.counts > 1)
-      Muon = Muon[twomuons]
-      D0 = D0[twomuons]
-      Dstar = Dstar[twomuons]
-      output['cutflow']['two muons']         += Muon.counts.sum()
-      output['cutflow']['D0 two muons']      += D0.counts.sum()
-      output['cutflow']['Dstar two muons']   += Dstar.counts.sum()
-
-      Dimuon = Muon.distincts()
-      output['cutflow']['all dimuons'] += Dimuon.counts.sum()
-
-      #opposite charge muons
-      opposite_charge = (Dimuon.i0['charge'] * Dimuon.i1['charge'] < 0)
-      Dimuon = Dimuon[opposite_charge]
-      output['cutflow']['opposite charge'] += Dimuon.counts.sum()
-
-      #same vtx or close in z
-      same_vtx = (Dimuon.i0['vtxIdx'] == Dimuon.i1['vtxIdx']) | (np.absolute(Dimuon.i0['z'] - Dimuon.i1['z']) < 0.2)
-      Dimuon = Dimuon[same_vtx]
-      output['cutflow']['same vtx'] += Dimuon.counts.sum()
-
-      # Only events with at least 1 dimuon
-      evtcut = (Dimuon.counts > 0)
-      Dimuon = Dimuon[evtcut]
-      D0 = D0[evtcut]
-      Dstar = Dstar[evtcut]
-
-      output['cutflow']['D0 evt cut'] += D0.counts.sum()
-      output['cutflow']['Dstar evt cut'] += Dstar.counts.sum()
-      
-      # Leading and Trailing muon separation
-      leading_mu = (Dimuon.i0.pt.content > Dimuon.i1.pt.content)
-      Muon_lead = JaggedCandidateArray.candidatesfromoffsets(Dimuon.offsets, 
-                                                       pt=np.where(leading_mu, Dimuon.i0.pt.content, Dimuon.i1.pt.content),
-                                                       eta=np.where(leading_mu, Dimuon.i0.eta.content, Dimuon.i1.eta.content),
-                                                       phi=np.where(leading_mu, Dimuon.i0.phi.content, Dimuon.i1.phi.content),
-                                                       mass=np.where(leading_mu, Dimuon.i0.mass.content, Dimuon.i1.mass.content),)
-
-      Muon_trail = JaggedCandidateArray.candidatesfromoffsets(Dimuon.offsets, 
-                                                       pt=np.where(~leading_mu, Dimuon.i0.pt.content, Dimuon.i1.pt.content),
-                                                       eta=np.where(~leading_mu, Dimuon.i0.eta.content, Dimuon.i1.eta.content),
-                                                       phi=np.where(~leading_mu, Dimuon.i0.phi.content, Dimuon.i1.phi.content),
-                                                       mass=np.where(~leading_mu, Dimuon.i0.mass.content, Dimuon.i1.mass.content),)
-
-      #Create the accumulators to save output
+      ############### Create the accumulators to save output
       columns = ['__fast_pt', '__fast_phi', '__fast_eta', '__fast_mass']
       muon_lead_acc = processor.dict_accumulator({})
       for var in columns:
@@ -199,11 +239,11 @@ class EventSelectorProcessor(processor.ProcessorABC):
       muon_trail_acc["nMuon"] = processor.column_accumulator(Muon_trail.counts)
       output["Muon_trail"] = muon_trail_acc
       
-      dimuon_acc = processor.dict_accumulator({})
+      dimu_acc = processor.dict_accumulator({})
       for var in columns:
-         dimuon_acc[var] = processor.column_accumulator(np.array(Dimuon[var].flatten()))
-      dimuon_acc["nDimuon"] = processor.column_accumulator(Dimuon.counts)
-      output["Dimuon"] = dimuon_acc
+         dimu_acc[var] = processor.column_accumulator(np.array(Dimu[var].flatten()))
+      dimu_acc["nDimu"] = processor.column_accumulator(Dimu.counts)
+      output["Dimu"] = dimu_acc
       
       D0_acc = processor.dict_accumulator({})
       for var in columns:
@@ -228,3 +268,4 @@ class EventSelectorProcessor(processor.ProcessorABC):
 
    def postprocess(self, accumulator):
       return accumulator
+    
