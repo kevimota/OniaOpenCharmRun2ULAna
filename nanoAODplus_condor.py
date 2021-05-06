7eb1171685e44b1d0d3d3c69878f0b84b84fa55a
Kevin Mota Amarilo <kevin.amarilo@cern.ch>
2021-05-06 05:19:41 -0300

    Changes for working with condor

---
+++ nanoAODplus_condor.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+
+import time
+import os, sys
+
+import coffea.processor as processor
+from coffea.nanoevents import BaseSchema
+
+from nanoAODplus_processor.EventSelectorProcessor import EventSelectorProcessor
+from tools.merger import merger
+from tools.plotter import plotter
+
+tstart = time.time()
+
+job = sys.argv[1]
+number = job.split('.')[-2].split('_')[-1]
+
+with open(job) as f:
+    file_list = f.read().splitlines()
+
+name = file_list[0].split('/')[-1].split('_')[0]
+name = name + '_' + number
+
+files = {name: file_list}
+
+print(name)
+
+# creating necessary folders into dir output data
+os.system("mkdir -p output/" + name)
+os.system("rm -rf output/" + name + "/*")          
+
+output = processor.run_uproot_job(files,
+                                treename='Events',
+                                processor_instance=EventSelectorProcessor(name),
+                                executor=processor.iterative_executor,
+                                executor_args={'schema': BaseSchema},
+                                chunksize=10000,
+                                )
+
+elapsed = round(time.time() - tstart, 2)
+print(f"Process finished in: {elapsed} s")
+
+merger(name)
+
+plotter(name)
+
+print("Transfering files...")
+os.system("cp output/" + name + "/* ../.")
