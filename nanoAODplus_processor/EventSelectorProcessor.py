cfcac4241fbcf9596b3efc32d4a7097072d60dd0
Kevin Mota Amarilo <kevimota@gmail.com>
2022-10-21 15:06:43 -0300

    Change the cut for the tracking on Dstar

--- nanoAODplus_processor/EventSelectorProcessor.py
+++ nanoAODplus_processor/EventSelectorProcessor.py
@@ -97,10 +97,16 @@ class EventSelectorProcessor(processor.ProcessorABC):
         Dstar = Dstar[(Dstar.KnValid > 4) & (Dstar.pinValid > 4) & (Dstar.KnPix > 1) & (Dstar.pinPix > 1)]
         output['cutflow']['Dstar trk hits cut'] += ak.sum(ak.num(Dstar))
 
-        Dstar = Dstar[(Dstar.Kdxy < 0.1) & (Dstar.pidxy < 0.1)]
+        D0 = D0[(D0.t1dxy < 0.5/(2 * np.arctan(np.exp(-D0.t1eta)))) & (D0.t2dxy < 0.5/(2 * np.arctan(np.exp(-D0.t2eta))))]
+        output['cutflow']['D0 trk dxy cut'] += ak.sum(ak.num(D0))
+
+        D0 = D0[(D0.t1dz < 0.5/(2 * np.arctan(np.exp(-D0.t1eta)))) & (D0.t2dz < 0.5/(2 * np.arctan(np.exp(-D0.t2eta))))]
+        output['cutflow']['D0 trk dz cut'] += ak.sum(ak.num(D0))
+
+        Dstar = Dstar[(Dstar.Kdxy < 0.5/(2 * np.arctan(np.exp(-Dstar.Keta)))) & (Dstar.pidxy < 0.5/(2 * np.arctan(np.exp(-Dstar.pieta))))]
         output['cutflow']['Dstar trk pt cut'] += ak.sum(ak.num(Dstar))
 
-        Dstar = Dstar[(Dstar.Kdz < 1) & (Dstar.pidz < 1)]
+        Dstar = Dstar[(Dstar.Kdz < 0.5/(2 * np.arctan(np.exp(-Dstar.Keta)))) & (Dstar.pidz < 0.5/(2 * np.arctan(np.exp(-Dstar.pieta))))]
         output['cutflow']['Dstar trk pt cut'] += ak.sum(ak.num(Dstar))
 
         # pis cuts
@@ -149,7 +155,7 @@ class EventSelectorProcessor(processor.ProcessorABC):
         D0 = D0[(D0.t1dxy < 0.5/(2 * np.arctan(np.exp(-D0.t1eta)))) & (D0.t2dxy < 0.5/(2 * np.arctan(np.exp(-D0.t2eta))))]
         output['cutflow']['D0 trk dxy cut'] += ak.sum(ak.num(D0))
 
-        D0 = D0[(D0.t1dz < .5/(2 * np.arctan(np.exp(-D0.t1eta)))) & (D0.t2dz < .5/(2 * np.arctan(np.exp(-D0.t2eta))))]
+        D0 = D0[(D0.t1dz < 0.5/(2 * np.arctan(np.exp(-D0.t1eta)))) & (D0.t2dz < 0.5/(2 * np.arctan(np.exp(-D0.t2eta))))]
         output['cutflow']['D0 trk dz cut'] += ak.sum(ak.num(D0))
 
         # D0 cosphi
